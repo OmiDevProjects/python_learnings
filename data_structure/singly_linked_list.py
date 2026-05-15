8a35cbe388792b896f2357e4bc4a89eaa261aca3
Onkar Nardekar <79778637+OmiDevProjects@users.noreply.github.com>
2026-05-15 06:38:15 +0000

    improved the code and fixed minor bugs

--- data_structure/singly_linked_list.py
+++ data_structure/singly_linked_list.py
@@ -1,6 +1,23 @@
 """ 
 Code: Singly Linked List
-Description: 
+Description:
+    Implemented a Singly Linked List data structure in Python.
+
+Features:
+    - Insert nodes at the beginning, end, or after a specific node
+    - Delete nodes by value, at the beginning or end
+    - Traverse and display list elements
+    - Search for elements in the list
+    - Calculate the length of the linked list
+
+Concepts Used:
+    - Object-Oriented Programming (OOP)
+    - Dynamic memory representation using nodes
+
+Purpose:
+    This project demonstrates the implementation and working
+    of a dynamic linear data structure without using Python's
+    built-in list methods.
 
 """
 
@@ -12,6 +29,7 @@ class Node:
 class SLL:
     def __init__(self, start=None):
         self.start = start
+        self.count = 0
 
     def is_empty(self):
         return self.start == None
@@ -21,6 +39,7 @@ class SLL:
         if not self.is_empty():
             node.next = self.start
         self.start = node
+        self.count += 1
 
     def insert_at_last(self, value):
         node = Node(item=value)
@@ -29,6 +48,9 @@ class SLL:
             while temp.next is not None:
                 temp = temp.next
             temp.next = node
+        else:
+            self.start = node
+        self.count += 1
 
     def insert_at(self, data, value):
         node = Node(item=data)
@@ -37,6 +59,7 @@ class SLL:
             if temp:
                 node.next = temp.next
                 temp.next = node
+                self.count += 1
     
     def show(self):
         if not self.is_empty():
@@ -57,16 +80,21 @@ class SLL:
     def delete_first(self):
         if not self.is_empty():
             if self.start.next == None:
-                self.start == None
+                self.start = None
             else:
                 self.start = self.start.next
-            
+            self.count -= 1
+
     def delete_last(self):
         if not self.is_empty():
-            temp = self.start
-            while temp.next.next is not None:
-                temp = temp.next
-            temp.next = None
+            if self.start.next == None:
+                self.start = None
+            else:
+                temp = self.start
+                while temp.next.next is not None:
+                    temp = temp.next
+                temp.next = None
+            self.count -= 1
     
     def delete_item(self, value):
         if not self.is_empty():
@@ -74,11 +102,15 @@ class SLL:
                 self.start = self.start.next
             else:
                 temp = self.start
-                while temp is not None:
+                while temp.next is not None:
                     if temp.next.item == value:
                         temp.next = temp.next.next
+                        self.count -= 1
+                        break
                     temp = temp.next
-
+            
+    def size(self):
+        return self.count
 
 
 s1 = SLL()
@@ -93,14 +125,18 @@ print('Inserting element at')
 s1.insert_at(34, 46)
 s1.show()
 
-# print('Deleting First Element')
-# s1.delete_first()
-# s1.show()
+print(s1.size())
+
+print('Deleting First Element')
+s1.delete_first()
+s1.show()
 
 # print('Deleting Last Element')
 # s1.delete_last()
 # s1.show()
 
-# print('Deleting Element for Linkedlist')
-# s1.delete_item(30)
-# s1.show()
+print('Deleting Element for Linkedlist')
+s1.delete_item(23)
+s1.show()
+
+print(s1.size())
